71d7363f36fa4242318144b1beb0df6b5355a18f
ripper-jc <matvey5i@ukr.net>
2025-01-13 18:28:04 +0200

    add SVG assets and update Next.js configuration for image optimization

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true, // Add this line to disable image optimization
+  },
 };
 
 export default nextConfig;
