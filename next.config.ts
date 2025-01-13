a199c9c0c330bd8c9d728b91a14a658db2a4a398
ripper-jc <matvey5i@ukr.net>
2025-01-13 19:13:10 +0200

    update Next.js configuration to set basePath for production and disable image optimization

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  distDir: 'dist',
+  basePath: isProd ? "/chessSchoolWebsite" : "",
+  output: "export",
+  distDir: "dist",
   images: {
     unoptimized: true, // Add this line to disable image optimization
   },
