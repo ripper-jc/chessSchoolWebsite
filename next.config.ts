f79a04c02ba786065a356ecb22fedcdf8f32ce73
ripper-jc <matvey5i@ukr.net>
2025-01-13 19:06:37 +0200

    add SSG and build manifest files; update Next.js configuration for image optimization

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  distDir: "dist",
+  output: 'export',
+  distDir: 'dist',
   images: {
     unoptimized: true, // Add this line to disable image optimization
   },
