4e7dee755cd2b3f12c22fe961e62f693fa99e12e
ripper-jc <matvey5i@ukr.net>
2025-02-19 23:06:10 +0200

    update Next.js configuration to change basePath for production environment

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,9 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: isProd ? "/chessSchoolWebsite" : "",
+  basePath: isProd ? "/chess-school-website-seven.vercel.app" : "",
   output: "export",
   distDir: "dist",
-  images: {
-    unoptimized: true, // Add this line to disable image optimization
-  },
 };
 
 export default nextConfig;
